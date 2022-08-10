f2cd7c1a518a0f2f31446124b09d68ced9848b0f
Radim Balusek <radim.balusek@seznam.cz>
2022-08-10 16:24:23 +0200

    first commit

---
+++ src/main/java/cz/balusek/linkedinmodule/LinkedInModuleApplication.java
@@ -0,0 +1,13 @@
+package cz.balusek.linkedinmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedInModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/balusek/linkedinmodule/LinkedInModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.balusek.linkedinmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
